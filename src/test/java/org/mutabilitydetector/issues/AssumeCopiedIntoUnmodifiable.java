65aa584f596f785efb64023e338e05529c55cd83
Grundlefleck <grundlefleck@gmail.com>
2012-12-19 23:21:52 +0000

    Extracted allowed reason into core jar. Change this to a test which shows intended usage of the allowed reason.

--- src/test/java/org/mutabilitydetector/issues/AssumeCopiedIntoUnmodifiable.java
+++ src/test/java/org/mutabilitydetector/issues/AssumeCopiedIntoUnmodifiable.java
@@ -1,49 +1,95 @@
 package org.mutabilitydetector.issues;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.mutabilitydetector.MutabilityReason;
-import org.mutabilitydetector.MutableReasonDetail;
-import org.mutabilitydetector.locations.FieldLocation;
+import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
+import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
+import static org.mutabilitydetector.unittesting.matchers.reasons.AssumeCopiedIntoUnmodifiable.assuming;
 
-public class AssumeCopiedIntoUnmodifiable extends BaseMatcher<MutableReasonDetail> {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
-    public static class Assuming {
-        private final String fieldName;
-        public Assuming(String fieldName) {
-            this.fieldName = fieldName;
-        }
-        public Matcher<MutableReasonDetail> isSafelyCopiedUnmodifiableCollectionWithImmutableTypes() {
-            return new AssumeCopiedIntoUnmodifiable(fieldName);
+import javax.annotation.concurrent.Immutable;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+/**
+ * The <code>AssumeCopiedIntoUnmodifiable</code> allowed reason is provided
+ * out-of-the-box since v0.9.
+ * <p>
+ * Since this class was linked to as an example of solving an issue, it now
+ * shows the intended usage.
+ * 
+ */
+public class AssumeCopiedIntoUnmodifiable {
+
+    /*
+     * A non-JDK collection type/constructor is used to perform the copy, before wrapping in
+     * an unmodifiable collection.
+     */
+    @Test
+    public void canAllowAnUnknownWrapperMethod() throws Exception {
+        assertInstancesOf(SafelyWrapsFieldWithUnknownMethod.class, 
+                          areImmutable(),
+                          assuming("myStrings").isSafelyCopiedUnmodifiableCollectionWithImmutableTypes());
+    }
+
+    @Immutable
+    public static final class SafelyWrapsFieldWithUnknownMethod {
+        public final List<String> myStrings;
+
+        public SafelyWrapsFieldWithUnknownMethod(Collection<String> myStrings) {
+            this.myStrings = Collections.unmodifiableList(Lists.newArrayList(myStrings));
         }
         
     }
 
-    public static AssumeCopiedIntoUnmodifiable.Assuming assuming(String fieldName) {
-        return new Assuming(fieldName);
+    /*
+     * Uses a non-JDK collection type, which is known to the developer, but not
+     * Mutability Detector be unmodifiable.
+     */
+    @Test
+    public void canAllowUsingACustomImmutableCollectionType() throws Exception {
+        assertInstancesOf(UsesImmutableAbstractCollectionType.class, 
+                          areImmutable(),
+                          assuming("myStrings").isSafelyCopiedUnmodifiableCollectionWithImmutableTypes());
+    }
+    
+
+    @Immutable
+    public static final class UsesImmutableAbstractCollectionType {
+        public final List<String> myStrings;
+
+        public UsesImmutableAbstractCollectionType(List<String> myStrings) {
+            this.myStrings = ImmutableList.<String>builder().addAll(myStrings).build();
+        }
     }
 
-    private final String fieldName;
-    
-    public AssumeCopiedIntoUnmodifiable(String fieldName) {
-        this.fieldName = fieldName;
+    /*
+     * Uses a mutable element type in a collection field. Should be used when
+     * the developer knows no elements are modified.
+     */
+    @Test
+    public void canAllowUnmodifiableListOfMutableElementType() throws Exception {
+        assertInstancesOf(SafelyWrappedAndCopiedWithMutableElementType.class, 
+                          areImmutable(),
+                          assuming("myDates").isSafelyCopiedUnmodifiableCollectionWithImmutableTypes());
     }
     
-    @Override
-    public void describeTo(Description description) { }
-
-    @Override
-    public boolean matches(Object arg0) {
-        MutableReasonDetail reasonDetail = (MutableReasonDetail) arg0;
-        if (reasonDetail.reason().isOneOf(MutabilityReason.COLLECTION_FIELD_WITH_MUTABLE_ELEMENT_TYPE)) {
-            String potentiallyAbstractField = ((FieldLocation) reasonDetail.codeLocation()).fieldName();
-            if (potentiallyAbstractField.equals(fieldName)) {
-                return true;
-            }
+    @Immutable
+    public static final class SafelyWrappedAndCopiedWithMutableElementType {
+        private final List<Date> myDates;
+        
+        public SafelyWrappedAndCopiedWithMutableElementType(List<Date> myDates) {
+            this.myDates = Collections.unmodifiableList(new ArrayList<Date>(myDates));
         }
         
-        return false;
+        public long timeOfFirstDate() {
+            return myDates.get(0).getTime();
+        }
     }
-
 }
