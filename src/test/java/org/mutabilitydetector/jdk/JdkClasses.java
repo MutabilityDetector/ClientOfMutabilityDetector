95d905d24ded57bb6a80d0d7c464d8f1b41db597
Graham Allan <graham.allan@timgroup.com>
2014-07-14 18:50:21 +0100

    Add test for jdk 8's Optional class.

---
+++ src/test/java/org/mutabilitydetector/jdk/JdkClasses.java
@@ -0,0 +1,17 @@
+package org.mutabilitydetector.jdk;
+
+import static org.mutabilitydetector.unittesting.AllowedReason.provided;
+import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
+import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class JdkClasses {
+
+	@Test
+	public void java_util_optional() throws Exception {
+		assertInstancesOf(Optional.class, areImmutable(), provided(Object.class).isAlsoImmutable());
+	}
+}
